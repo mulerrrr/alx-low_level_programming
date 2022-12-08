e18ce246fa9833680dbcdf49d566aac05e029615
mulerrrr <mulerrrr4@gmail.com>
2022-12-08 13:23:33 -0800

    vz

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -13,5 +13,7 @@
 int main(void)
 {
    printf("with proper grammar, but the outcome is a piece of art,,");
-   return(0);
+              
+              return(0);
+
 }   
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *  *
+ *
+ *  * Return: Always 0 (Success)
+ * 
+ */
+
+int main(void)
+
+{
+      printf("Size of char: %zu byte\n", sizeof(char));
+      printf("Size of int: %zu bytes\n", sizeof(int));
+      printf("Size of float: %zu bytes\n", sizeof(float));
+      printf("Size of double: %zu bytes\n", sizeof(double));
+	    return (0);
+
+}
