cc99f4cbbb1833151aa20eb1db70ce3796d81271
mulerrrr <mulerrrr4@gmail.com>
2022-12-11 06:45:59 -0800

    cd

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints numbers from 0 to 9
- * Return: Always 0
+ * main - prints single digits in base 10
+ * Return:Always 0
  */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d", num);
-	printf("\n");
+		putchar((num % 10) + '0');
+	putchar('\n');
 	return (0);
 }
