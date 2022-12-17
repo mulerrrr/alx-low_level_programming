03d6a81043859500b3cd06ac60ed9ef46e671496
mulerrrr <mulerrrr4@gmail.com>
2022-12-17 13:14:06 -0800

    f

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 /**
  * _isupper - checks for uppercase character
@@ -8,13 +7,12 @@
  */
 int _isupper(int c)
 {
-     if (c >= 'A' && c <= 'Z')
-   {
-        return (1);
-   }
-    else
-  {
-        return (0);
-  }
-
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
---
+++ 0x04-more_functions_nested_loops/d.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *
+ */
+int main(void)
+{
+	int x;
+	
+	for (x = 1; x < 5; x++)
+	{
+		printf("%d \n", x);
+	}
+	return (0);
+}
