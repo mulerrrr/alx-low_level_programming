3584b592233365c25087dfda4d402b634f766b9f
mulerrrr <mulerrrr4@gmail.com>
2022-12-19 03:38:30 -0800

    f

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98-Function takes a pointer to an integer parameter
+ * int n is an intger pointer
+ * Return:0
+ */
+void reset_to_98(int *n)
+{
+	/* n is intger pointer*/
+	*n = 98;
+}
