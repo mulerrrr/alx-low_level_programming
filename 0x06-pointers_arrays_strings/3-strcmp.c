28031c0f9c104f2cb687320340344d2e542ef91c
mulerrrr <mulerrrr4@gmail.com>
2022-12-22 10:51:25 -0800

    h

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,18 +3,18 @@
  * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
- * Return: s1[1] - s2[1]
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s1[1] != '\0' && s2[1] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[1] != s2[1])
+		if (s1[i] != s2[i])
 		{
-			return (s1[1] - s2[1]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
