020824e73f4750c547c9d2cb84838262a943ce43
mulerrrr <mulerrrr4@gmail.com>
2023-01-02 12:29:47 -0800

    re

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,7 +11,7 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; s[inde] >= '\0'; index++)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[inde] == c)
 			return (s + index);
