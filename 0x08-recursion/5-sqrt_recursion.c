b328c04208543f9a9392aba7e8b367f16479fc3e
mulerrrr <mulerrrr4@gmail.com>
2023-01-04 07:42:41 -0800

    t

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "mian.h"
+/**
+ * _evaluate - evaluate function sqrt
+ * @i: integer
+ * @n: integer
+ * Return: evalute sqrt
+ */
+int _evaluate(int i, int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i + i < n)
+		return (_evalute(i + 1, n));
+	else if (i * i == n)
+		return (i);
+	return (-1);
+	return (-1);
+}
+/**
+ * _sqrt_recursion - evalute sqrt
+ * @n: integer
+ * Return: sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+		return (_evalute(i, n));
+}
