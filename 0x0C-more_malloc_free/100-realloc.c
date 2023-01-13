d6e42fd6aa00f46a564c84f3d23ec5c1399818e4
mulerrrr <mulerrrr4@gmail.com>
2023-01-13 11:40:07 -0800

    qw

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,16 +16,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 	else
 	return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
 
 	if (new_size == 0 && ptr != NULL)
-		return (ptr);
+		free (ptr);
 	return (0);
 	relloc = malloc(new_size);
 	if (relloc == NULL)
 		return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relloc + i) - clone[i];
+		*(relloc + i) = clone[i];
 	}
 	free(ptr);
 	return (relloc);
