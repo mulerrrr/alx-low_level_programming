e3981a32f76d0054b6e4518ecc63e3fd129f18b1
mulerrrr <mulerrrr4@gmail.com>
2023-01-26 13:23:58 -0800

    g

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,21 @@
-# include "lists.h"
+#include "lists.h"
+
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: list.
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- * Return: number of nodes in the list.
+ * Return: The number of elements in h.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t i = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		elements++;
 		h = h->next;
-		i++;
 	}
-	return (i);
+
+	return (elements);
 }
