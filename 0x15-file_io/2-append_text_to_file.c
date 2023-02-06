052e004ba98065242a581c52150c80780aec8d27
mulerrrr <mulerrrr4@gmail.com>
2023-02-06 13:40:04 -0800

    f

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,68 +2,45 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  * @argc: The argument count
-  * @argv: The argument vector
+  * append_text_to_file - ...
+  * @filename: ...
+  * @text_content: ...
   *
   * Return: ...
   */
-int main(int argc, char **argv)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	if (argc != 3)
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
 	}
 
-	copy_file(argv[1], argv[2]);
-	exit(0);
+	close(fd);
+	return (1);
 }
 
 /**
-  * copy_file - ...
-  * @src: ...
-  * @dest: ...
+  * _strlen - Returns the length of a string
+  * @s: String to count
   *
-  * Return: ...
+  * Return: String length
   */
-void copy_file(const char *src, const char *dest)
+int _strlen(char *s)
 {
-	int ofd, tfd, readed;
-	char buff[1024];
+	int c = 0;
 
-	ofd = open(src, O_RDONLY);
-	if (!src || ofd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		exit(98);
-	}
+	while (s[c])
+		c++;
 
-	tfd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((readed = read(ofd, buff, 1024)) > 0)
-	{
-		if (write(tfd, buff, readed) != readed || tfd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-			exit(99);
-		}
-	}
-
-	if (readed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		exit(98);
-	}
-
-	if (close(ofd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ofd);
-		exit(100);
-	}
-
-	if (close(tfd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfd);
-		exit(100);
-	}
+	return (c);
 }
